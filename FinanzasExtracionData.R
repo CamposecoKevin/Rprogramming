1d1121c319cbf43de23ec698e4f41327e813d533
CamposecoKevin <rubelibolo@gmail.com>
2020-10-17 23:28:43 -0600

    Archivo para descargar datos financieros de acciones de las empresas contizadas en bolsa.

---
+++ FinanzasExtracionData.R
@@ -0,0 +1,23 @@
+#instalacción
+install.packages("quantmod")
+library(quantmod)
+library(TTR)
+library(zoo)
+
+
+#descargar datos todos
+getSymbols("AAPL")
+
+#descargar cierta fecha
+getSymbols("AAPL", from = "2005-01-02", to="2010-12-31")
+
+
+#Creación de grafica
+chartSeries(AAPL, theme = "white")
+
+
+#para descargar datos desde google.
+
+getSymbols("INTC", src = "google")
+
+
