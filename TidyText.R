c78eb2b343565c0f38e2a086df67f2a84596a207
camposecokevin <rubelibolo@gmail.com>
2021-05-04 22:55:19 -0600

    Tidytex
    Revisión de data set

---
+++ TidyText.R
@@ -0,0 +1,24 @@
+install.packages("tidytext")
+library(tidytext)
+library(tidyverse)
+
+
+#PRIMER CASO DE ANALISIS DE CHARLAS TEDs
+
+# ver como esta la estructura de los datos.
+glimpse(mtcars)
+
+summary(mtcars)
+
+#ver en tablas la data set
+view(mtcars)
+
+?mtcars
+#Ver la información de dataset escrito.
+
+??glimpse
+
+??tidytext
+
+
+
