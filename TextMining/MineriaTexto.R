a763e254431d044037b2700874ff2ee8c151a78f
Kevin Camposeco <67613124+CamposecoKevin@users.noreply.github.com>
2021-05-07 08:10:45 -0600

    Update MineriaTexto.R
    Mineria de textos procedimientos y funcionalidades de tm

---
+++ TextMining/MineriaTexto.R
@@ -0,0 +1,67 @@
+```{r,eval=FALSE}
+library(rtweet)
+tw<-search_tweets("Guatemala",n=500,include_rts = F)
+tw$text
+```
+
+```{r,eval=FALSE}
+library(tm)
+library(dplyr)
+setwd("C:\\Users\\ALVARO\\Documents\\GitHub\\EST-384\\data")
+fb<-read.csv("bd_sc.csv")
+#aqui te permite colocar los simbolos
+fb<-read.csv("bd_sc.csv",encoding = "UTF-8")
+#fb<-read.csv("bd_sc.csv",encoding = "Latin-1")
+fb$post_text[5]
+```
+
+### Colección de documentos PDFs
+
+```{r,eval=FALSE}
+library(pdftools)
+dir<-"C:\\Users\\ALVARO\\Documents\\GitHub\\EST-384\\data\\pdf"
+pdfdocs <- VCorpus(DirSource(dir, pattern = ".pdf"), 
+                   readerControl = list(reader = readPDF))
+```
+
+```{r,eval=FALSE}
+library(tm)
+#Remover la puntuación
+removePunctuation(x)
+#quitar espacios si existen varios, a uno solo
+stripWhitespace(x)
+#quita todo los número que esta
+removeNumbers(x)
+#remueve palabras en un vector
+removeWords(x,c("Hola","hoy"))
+x
+#stopwods donde los artículos se quitan, que tiene version en español.
+removeWords(x,stopwords("es")[-47])
+
+
+```{r,eval=FALSE}
+x<-"Hola a todos, ¿Cómo estan?.    hoy es 30 de Noviembre"
+#nchar nos muestra todos los caracteres almacenados incluidos espacios
+nchar(x)
+#Sirve para concatenar
+paste("Pregunta 1:",x,"Fin de la oración")
+#
+paste0("Pregunta 1:",x,"Fin de la oración")
+#Reemplazar elementos en un vector
+gsub("Hola","xxx",x)
+#Es un buscador, decirme que ya lo ha encontrado en los diferentes espacios de trabajo
+grep("Hola",x)
+
+y<-c("Hola a todos","chau","Hola")
+grep("Hola",y)
+#todo minisculo
+tolower(x)
+#todo mayuscula
+toupper(x)
+
+library(qdap)
+fake.text<-'R text mining is good but text mining in python is also'
+patterns<-c('good','also','text mining')
+replacements<-c('great','just as suitable','tm')
+mgsub(patterns,replacements,fake.text)
+```
