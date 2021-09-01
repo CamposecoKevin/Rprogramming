d01674a846694b4c24d139850430c5ded78db2e3
camposecobusiness <67613124+CamposecoKevin@users.noreply.github.com>
2021-08-31 22:01:19 -0600

    Data R1 Productores
    New data about farmers of the R1

---
+++ Productores_r1/Productores_r1_20-21.R
@@ -0,0 +1,22 @@
+library(tidyverse)
+library(readxl)
+library(dplyr)
+library(writexl)
+
+
+#Establecer el directorio de trabajo
+setwd("C:/Users/KEVIN CAMPOSECO/Documents/Kevin_Proyecto/Rprogramming/Productores_r1/LuisMendoza/")
+
+
+#Cargar Archivo
+ProductoresR1<-read_xlsx("C:/Users/KEVIN CAMPOSECO/Downloads/Listado de Productores Dueños con Atencion 2021.xlsx")
+View(ProductoresR1)
+
+
+#filtro por técnicos
+tecnico<-filter(ProductoresR1,ProductoresR1$`Usuario que Registro` =="Salvador Timoteo Ventura Ajpop")
+
+#guardar datos
+write_xlsx(tecnico,"SalvadorVentura.xlsx")
+
+
