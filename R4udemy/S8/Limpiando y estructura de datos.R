5057adbcec512d7c6042d70ac2bab1c94ff0117f
camposecobusiness <67613124+CamposecoKevin@users.noreply.github.com>
2022-05-26 17:29:44 -0600

    Update for S8 more infomation

--- R4udemy/S8/Limpiando y estructura de datos.R
+++ R4udemy/S8/Limpiando y estructura de datos.R
@@ -1,25 +1,70 @@
 
 library(tidyverse)
 library(readxl)
+library(writexl)
 
 
 
 # 89- fichero de datos limpio ---------------------------------------------
 
 
+View(population)
+
+poblacion <- population %>%
+  filter(country %in% c("Guatemala", "Chile", "Honduras"),
+         year %in% c(1999,2000))
+
+view(poblacion)  
+
+
+#Población por año
+
+poblacion %>%
+  count(year, wt = population)
+  
+  
+# 90-Spreading y gathering ------------------------------------------------
+
+eventos<- read_csv("C:/Users/KEVIN CAMPOSECO/Downloads/Eventos_20220525_204426/query_1.csv")
+
+
+
+view(eventos)
+
+str(eventos)
+
+rogelio <- eventos %>%
+  filter(eventos$`Id Técnico` == 137)
+
+view(rogelio)
+
+write_csv(eventos, "eventos.csv")
+
+
+setwd("C:/Users/KEVIN CAMPOSECO/Mis documentos/Kevin_Proyecto/Rprogramming/R4udemy/S8/")
+
+
+
+
+
+  
+  
+
+
+
+
+
+
+
 
   
   
   
   
   
-  
-  
-  
-  
-  
-  
-  
+
+
+
 
   
 
