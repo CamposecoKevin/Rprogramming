db8a064481db17ba44de11d0515101ab5c425de7
camposecokevin <rubelibolo@gmail.com>
2022-07-07 01:17:56 -0600

    Update date for visualition

--- Coursera/Explory data/week1.R
+++ Coursera/Explory data/week1.R
@@ -1,4 +1,36 @@
 
-# Graphics ----------------------------------------------------------------
+# 01-Graphics ----------------------------------------------------------------
+
+#the use of graphs is very important at the time data 
+#analyst
+
+
+# Exploratory Graphs (part 1) ---------------------------------------------
+population <- data.frame(population)
+
+#¿Qué paises tiene mayor población que el promedio?
+
+#un resumen de los datos
+
+ResumenPoblacion<- summary(population$population)
+
+#gráfica de bigote
+
+boxplot(population$population, col= "red")
+
+
+filter(population,population$population >= promedio )
+
+str(population)
+
+
+
+
+
+
+
+
+
+
 
 
