7a0135e3de93a6ddbb6e2d6083244b709f338e0f
camposecokevin <rubelibolo@gmail.com>
2022-07-14 23:10:28 -0600

    update for rprograming

--- R4udemy/S8/Limpiando y estructura de datos.R
+++ R4udemy/S8/Limpiando y estructura de datos.R
@@ -40,18 +40,28 @@ poblacion %>%
 # 91-técnica de gathering ----------------------------------------------------
 
 
+new_table4a<-table4a %>%
+  gather(`1999`, `2000`, key = "year", value = "cases" )
 
 
 
+new_table4b<-table4b %>%
+  gather(`1999`, `2000`, key = "year", value = "population" )
+new_table4b
+
+# unir distitntos archivos
+#Para que esto funcione tienen que tener las mismas filas
+Union_table_ab<-left_join(new_table4a,new_table4b)
 
 
 
+# 92. La técnica de spresading --------------------------------------------
 
+#convertir filas a columnas, de acuerdo a la variable a utilizar.
+new_table2<- table2 %>%
+  spread(key = type, value = count)
 
-
-
-
-
+table2
 
 
 
