105645bf9c6c58451c4e9b26035e68da982d3260
camposecobusiness <harli.camposeco@yahoo.com>
2023-02-03 16:02:07 -0600

    Empezando con lo strings

--- R4udemy/S9/Manejo de strings, factores y fechas.R
+++ R4udemy/S9/Manejo de strings, factores y fechas.R
@@ -5,4 +5,70 @@
 
 # 111-Strings -------------------------------------------------------------
 
+#welcome this class.
+
+
+
+# 112 los fundamentos del String -----------------------------------------------------
+library(stringr)
+
+s1 <- "estos es un string"
+
+#Para cerrar, se debe agregar otra comilla doble
+backslah <- "\""
+
+#En si se usa comillas simples, no es necesario usar otro al final
+single_quote <-  ' / '
+
+#para juntarlos
+x <- c(s1, backslah, single_quote)
+#sirve para encontrar que simbolos contienen
+writeLines(x)
+
+#caracteres escapantes
+
+#?'"'
+mu<- '\u00b5'
+
+mu
+
+#para ver todos los caracteres,
+#de como se usan.
+?'"'
+
+
+
+# 113 operaciones con string ----------------------------------------------
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
