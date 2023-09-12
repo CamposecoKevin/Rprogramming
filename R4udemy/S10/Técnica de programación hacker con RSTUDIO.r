9835c2b341a22906823676743b7b01d9c4f0d877
CamposecoKevin <rubelibolo@gmail.com>
2023-09-11 21:25:58 -0600

    update tecnics for programming

--- R4udemy/S10/Técnica de programación hacker con RSTUDIO.r
+++ R4udemy/S10/Técnica de programación hacker con RSTUDIO.r
@@ -2,3 +2,65 @@
 # 133 técnicas de programdor profesional ----------------------------------
 
 
+
+# 134 pipes ---------------------------------------------------------------
+
+
+#sobre escribir la variable original
+
+#componer funciones
+
+# usar una pipe
+
+#assing, get, load
+
+
+# 134 pipes alternantivas -------------------------------------------------
+
+
+rnorm(1000) %>% 
+  matrix(ncol = 2) %T>%
+  plot() %>% 
+  str()
+
+
+# correlación
+
+mtcars %$%
+  cor(disp, mpg)
+  
+
+mtcars %<>%
+  filter(cyl == 6)
+
+
+# 136 creación de funciones -----------------------------------------------
+
+# nombre de la funcion
+
+
+# 137 cuando conviene crear funciones -------------------------------------
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
