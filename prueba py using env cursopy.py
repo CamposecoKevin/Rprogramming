31a90947f7c5c06ab526cc63c43a85e0214e510d
CamposecoKevin <rubelibolo@gmail.com>
2023-10-30 00:12:57 -0600

    the firts code to python using env cursopy

---
+++ prueba py using env cursopy.py
@@ -0,0 +1,18 @@
+import matplotlib.pyplot as plt
+
+eje_X =['Py','R','Node']
+
+eje_y = [50,20,35]
+
+plt.bar(eje_X, eje_y)
+
+plt.ylabel("Cantidad de ususario")
+plt.xlabel("Lenguajes de programación")
+plt.title("Usuaarios de lenguaje de programación")
+
+plt.show()
+plt.figure()
+
+
+import numpy as np
+
